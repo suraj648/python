8cedf6c34ff03f5e36ed328b8f522111d283ea32
suraj648 <sooraj.001kumar@gmail.com>
2024-09-27 16:28:33 +0530

    new function

---
+++ start.py
@@ -0,0 +1,38 @@
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print(f"{num} is even number...")
+else:
+    print(f"{num} is odd number...")
+
+
+
+
+def palindrome(string):
+    temp = ''
+    for i in string:
+        temp = i + temp
+
+    if string == temp:
+        return "palindrome"
+    else:
+        return "not palindrome"
+    
+
+
+string = input("Enter a string: ")
+print(palindrome(string))
+
+
+
+def sqrt(number):
+    return number ** 2 ,number **3
+    
+number = int(input("enter a number"))
+
+print(sqrt(number))
+
+
+def greet():
+    print("good afternoon")
+    print("had your lunch")
